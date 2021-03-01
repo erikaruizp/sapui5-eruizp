a27cd9d17081be914aaf008c78febc717a2239f8
erikaruizp <erikaruizp@gmail.com>
2021-03-01 23:21:17 +0000

    08_Binding_y_Formateadores

---
+++ Invoices/SAPUI5/webapp/model/InvoicesFormatter.js
@@ -0,0 +1,19 @@
+//@ts-nocheck
+sap.ui.define([
+   
+], 
+function () {
+    "use strict";
+    return {
+        invoiceStatus: function(estado) {
+            const resourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            switch (estado) {
+                case 'A': return resourceBundle.getText("invoiceStatusA");
+                case 'B': return resourceBundle.getText("invoiceStatusB");
+                case 'C': return resourceBundle.getText("invoiceStatusC");
+                default: return estado;              
+            }
+        }
+
+    };
+});    
