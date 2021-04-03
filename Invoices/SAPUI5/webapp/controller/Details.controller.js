7c7e1c850dfcbdda0a12b9b543bc9a4ac60ff182
erikaruizp <erikaruizp@gmail.com>
2021-04-03 22:32:54 +0000

    13_Controles_Personalizados

--- Invoices/SAPUI5/webapp/controller/Details.controller.js
+++ Invoices/SAPUI5/webapp/controller/Details.controller.js
@@ -2,19 +2,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History",
-    "sap/ui/core/UIComponent"
+    "sap/ui/core/UIComponent",
+    "sap/m/MessageToast"
 ], 
 /**
  * 
  * @param {typeof sap.ui.core.mvc.Controller} Controller 
  * @param {typeof sap.ui.core.routing.History} History 
  * @param {typeof sap.ui.core.UIComponent} UIComponent 
+ * @param {typeof sap.m.MessageToast} MessageToast
  *
  */
-function (Controller,History,UIComponent) {
+function (Controller,History,UIComponent,MessageToast) {
 
     return Controller.extend("logaligroup.SAPUI5.Component", {
-        _onObjetcMatch: function(oEvent) {
+        _onObjectMatch: function(oEvent) {
+            this.byId("rating").reset();
             this.getView().bindElement({
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "northwind"
@@ -22,7 +25,7 @@ function (Controller,History,UIComponent) {
         },
         onInit: function() {
             const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.getRoute("Details").attachPatternMatched(this._onObjetcMatch, this)
+            oRouter.getRoute("Details").attachPatternMatched(this._onObjectMatch, this)
         },
         onNavBack: function () {
             const oHistory = History.getInstance();
@@ -34,6 +37,11 @@ function (Controller,History,UIComponent) {
                 const oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("RouteApp",{},true);
             }
+        },
+        onRatingChange: function (oEvent) {
+            const fValue = oEvent.getParameter("value");
+            const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            MessageToast.show(oResourceBundle.getText("productRatingMessage",fValue)); 
         }   
 	});
 });
